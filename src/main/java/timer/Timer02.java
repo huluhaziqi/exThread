5183a864e44eba32017e10f9e1b926e930d9ab7e
huluhaziqi <huluhaziqi@gmail.com>
2018-02-24 15:40:48 +0800

    add timer

--- src/main/java/timer/Timer02.java
+++ src/main/java/timer/Timer02.java
@@ -12,17 +12,37 @@ public class Timer02 {
     static class MyTask02 extends TimerTask {
         @Override
         public void run() {
-            System.out.println("运行时间是 ： " + new Date());
+            System.out.println("A运行时间是 ： " + new Date());
+            try {
+                Thread.sleep(2000);
+                //将自己从timeTask队列中清除
+                this.cancel();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    static class MyTask03 extends TimerTask {
+        @Override
+        public void run() {
+            System.out.println("B运行时间是 ： " + new Date());
+            try {
+                Thread.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     public static void main(String[] args) {
         MyTask02 myTask = new MyTask02();
+        MyTask03 myTask2 = new MyTask03();
         SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
         String dateString = "2018-02-24 15:16:00";
         try {
             System.out.println("当前时间" + new Date());
             timer.schedule(myTask,dateFormat.parse(dateString),2000);
+            timer.schedule(myTask2,dateFormat.parse(dateString),2000);
         } catch (ParseException e) {
             e.printStackTrace();
         }
